308b0f90c039705fe5ee94a9a152eae66f0c63b4
henry <chunhinhenry@gmail.com>
2024-04-26 16:45:12 -0400

    added route

--- server/frontend/src/components/Dealers/Dealers.jsx
+++ server/frontend/src/components/Dealers/Dealers.jsx
@@ -31,7 +31,7 @@ const Dealers = () => {
       method: "GET"
     });
     const retobj = await res.json();
-    if(retobj.status === 200) {
+    if (retobj.status === 200 && retobj.dealers) {
       let all_dealers = Array.from(retobj.dealers)
       let states = [];
       all_dealers.forEach((dealer)=>{
@@ -63,7 +63,7 @@ return(
       <select name="state" id="state" onChange={(e) => filterDealers(e.target.value)}>
       <option value="" selected disabled hidden>State</option>
       <option value="All">All States</option>
-      {states.map(state => (
+      {states && states.map(state => (
           <option value={state}>{state}</option>
       ))}
       </select>        
